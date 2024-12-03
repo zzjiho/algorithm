feb2c4f8489fd5d8bcbf2b8cb008b2a17c754a4b
zzjiho <109573487+zzjiho@users.noreply.github.com>
2024-12-03 17:48:58 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1048 ms, Memory: 272228 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -3,6 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+class Edge implements Comparable<Edge> {
+    int vertex;
+    int cost;
+
+    Edge(int vertex, int cost) {
+        this.vertex = vertex;
+        this.cost = cost;
+    }
+
+    public int compareTo(Edge ob) {
+        return this.cost - ob.cost;
+    }
+}
+
 // n: 번까지 도시
 // m: 단방향 수
 // k: 최단거리
@@ -15,18 +29,18 @@ public class Main {
     static int[] dis;
 
     static void BFS(int x) {
-        Queue<Integer> Q = new LinkedList<>();
+        Queue<Edge> Q = new LinkedList<>();
         visited[x] = true; // 시작점 방문체크
         dis[x] = 0; // 시작점 거리 0
-        Q.offer(x);
+        Q.offer(new Edge(x,1)); // 가중치 1
 
         while (!Q.isEmpty()) {
-            int cur = Q.poll();
-            for (int adj : graph[cur]) { // 인접그래프
+            Edge cur = Q.poll();
+            for (int adj : graph[cur.vertex]) { // 인접그래프
                 if (!visited[adj]) {
-                    dis[adj] = dis[cur] + 1; // 거리 업데이트
+                    dis[adj] = dis[cur.vertex] + 1; // 거리 업데이트
                     visited[adj] = true; // 방문체크
-                    Q.offer(adj); // 큐에 담기
+                    Q.offer(new Edge(adj, 1)); // 큐에 담기
                 }
             }
         }
