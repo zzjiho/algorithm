aab896611ac6b0fe0142ded66c569583a661f9c8
zzjiho <109573487+zzjiho@users.noreply.github.com>
2024-12-05 16:25:46 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 조합구하기
+public class Main {
+
+    static int n, m;
+    static int[] answer;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    static void DFS(int L, int s) {
+        if (L == m) {
+            for (int x : answer) {
+                sb.append(x).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int i = s; i <= n; i++) {
+                answer[L] = i;
+                DFS(L + 1, i + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        answer = new int[m];
+        visited = new boolean[n+1];
+
+        DFS(0, 1);
+        System.out.println(sb);
+    }
+}
