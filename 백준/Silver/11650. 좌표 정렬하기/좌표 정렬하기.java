cb18e476082591c89695b1f75c0de182d4bc9dba
zzjiho <109573487+zzjiho@users.noreply.github.com>
2024-12-17 21:35:25 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1400 ms, Memory: 60112 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Point implements Comparable<Point> {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int compareTo(Point o) {
+        if (this.x == o.x) {
+            return this.y - o.y;
+        } else {
+            return this.x - o.x;
+        }
+    }
+}
+
+public class Main {
+
+    static void solution(ArrayList<Point> ar) {
+        Collections.sort(ar);
+        for (Point x : ar) {
+            System.out.println(x.x + " " + x.y);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Point> ar = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            ar.add(new Point(x, y));
+        }
+        solution(ar);
+    }
+}
