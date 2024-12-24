8091b9d31a626d66fec23ce211f6c11f868b3249
zzjiho <109573487+zzjiho@users.noreply.github.com>
2024-12-25 01:02:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,46 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
-    static int dp[];
     static int INF = 9999;
-
-    static int solution(int n) {
-
-        if (n < 0) {
-            return INF;
-        }
-
-        if (n == 0) {
-            return 0;
-        }
-
-        if (dp[n] != 0) {
-            return dp[n];
-        }
-
-        int use3 = solution(n - 3) + 1;
-        int use5 = solution(n - 5) + 1;
-
-        dp[n] = Math.min(use3, use5);
-
-        return dp[n];
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        dp = new int[n+1];
+        // dp[i] = i kg 설탕을 최소 봉지로 가져가는 수
+        // 1. 테이블 세팅
+        int[] dp = new int[n + 1];
 
-        int answer = solution(n);
+        // 2. 초기화 세팅
+        Arrays.fill(dp, INF);
+        dp[0] = 0; // 0kg 은 봉지를 안쓴다.
 
-        if (answer >= INF) {
-            System.out.println(-1);
-        } else {
-            System.out.println(answer);
+        // 3. 점화식 구현
+        for (int i = 1; i <= n; i++) {
+            // 3kg 봉지 쓸 수 있는 경우
+            if (i - 3 >= 0 && dp[i - 3] != INF) {
+                dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+            }
+
+            // 5kg 봉지 쓸 수 있는 경우
+            if (i - 5 >= 0 && dp[i - 5] != INF) {
+                dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+            }
         }
+
+        System.out.println((dp[n] == INF) ? -1 : dp[n]);
     }
 }
