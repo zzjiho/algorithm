a92f3b17232b3fee336fb66d4370223e4665545e
zzjiho <109573487+zzjiho@users.noreply.github.com>
2024-12-31 15:07:29 +0900

    [Silver I] Title: 양, Time: 160 ms, Memory: 19292 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.java
+++ 백준/Silver/3184. 양/양.java
@@ -3,71 +3,75 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+// 늑대,양 세는것은 counts 배열 같은곳에 담아서 세는게 좋음
+// 확장성을 고려해야함. 동물이 늘어나면 복잡해짐.
+class Main {
 
-    static int r,c; //행열
+    static int r,c; // 행열
+    static int wolf,sheep;
+    static int wolfTotal,sheepTotal;
     static char[][] board;
     static boolean[][] visited;
+
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int totalSheep = 0;
-    static int totalWolf = 0;
 
-    // DFS로 영역을 탐색하고 양,늑대의 수를 센다.
-    static void DFS(int x, int y, int[] counts) {
+    static void DFS(int x, int y) {
         visited[x][y] = true;
 
         if (board[x][y] == 'o') {
-            counts[0]++;
+            sheep++;
         } else if (board[x][y] == 'v') {
-            counts[1]++;
+            wolf++;
         }
 
         for (int i = 0; i < 4; i++) {
             int nx = dx[i] + x;
             int ny = dy[i] + y;
-
-            if (nx >= 0 && nx < r && ny >= 0 && ny < c) { // 바운더리내에서 울타리 아닌곳 이동가능
+            // 바운더리내 방문하지 않으면서, 양이나 늑대인경우
+            if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
                 if (!visited[nx][ny] && board[nx][ny] != '#') {
-                    DFS(nx, ny, counts);
+                    DFS(nx, ny);
                 }
             }
         }
     }
 
-    // 모든 영역 탐색 및 생존한 양,늑대 세기
     static void solution() {
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
+                // 방문하지 않은곳 탐색가능
                 if (!visited[i][j] && board[i][j] != '#') {
-                    int[] counts = new int[2]; // 새로운 영역마다 count 초기화, [0] = 양, [1] = 늑대
-                    DFS(i, j, counts);
-                    if (counts[0] > counts[1]) { // 양이 더 많으면 늑대 몰살
-                        totalSheep += counts[0];
-                    } else {
-                        totalWolf += counts[1]; // 늑대 >= 양 이면 양 몰살
+                    wolf = 0; sheep = 0;
+                    DFS(i, j);
+                    if (wolf >= sheep) {
+                        wolfTotal += wolf;
+                    } else if (sheep > wolf) {
+                        sheepTotal += sheep;
                     }
                 }
             }
         }
     }
 
+    // 바운더리내 갈수있는 영역에서 양vs늑대 로 cnt 출력하기
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken()); // 6
-        c = Integer.parseInt(st.nextToken()); // 6
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
         board = new char[r][c];
         visited = new boolean[r][c];
 
         for (int i = 0; i < r; i++) {
-            String line = br.readLine();
+            String str = br.readLine();
             for (int j = 0; j < c; j++) {
-                board[i][j] = line.charAt(j);
+                board[i][j] = str.charAt(j);
             }
         }
 
         solution();
-        System.out.println(totalSheep + " " + totalWolf);
+        System.out.print(sheepTotal + " " + wolfTotal);
     }
 }
