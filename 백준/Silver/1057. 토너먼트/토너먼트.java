3c2f4e7072cac95618477ac1e9ad46784ae9e8d9
zzjiho <109573487+zzjiho@users.noreply.github.com>
2025-01-16 22:39:39 +0900

    [Silver IV] Title: 토너먼트, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static void solution(int N, int a, int b) {
+        int answer = 0;
+
+        while (a != b) {
+            a = a / 2 + a % 2;
+            b = b / 2 + b % 2;
+            answer++;
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        solution(N, a, b);
+    }
+}
